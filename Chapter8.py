bd7897707e5713cf6c84b4f39e2e197a64da07e4
Jimmply <etozhejimmy@gmail.com>
2022-04-14 17:24:57 +0300

    finish chapt8

--- Chapter8.py
+++ Chapter8.py
@@ -37,9 +37,25 @@ def getContours(img):
         area = cv2.contourArea(cnt)
         print(area)
         if area>500:
-            cv2.drawContours(imgContour, cnt, -1,(255,0,0),3)
+            cv2.drawContours(imgContour, cnt, -1,(0,0,0),3)
             peri = cv2.arcLength(cnt, True)
-            print(peri)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            #print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor ==3: ObjectType = "Tri"
+            elif objCor ==4:
+                aspRatio = w/float(h)
+                if aspRatio > 0.95 and aspRatio<1.05:ObjectType="Square"
+                else: ObjectType="Rectangle"
+            elif objCor > 4:ObjectType="Circles"
+            else: ObjectType="None"
+
+            cv2.rectangle(imgContour,(x, y),(x + w, y + h),(0,0,0),2)
+            cv2.putText(imgContour, ObjectType, (x+(w//2)-10,y+(h//2)-10),
+                        cv2.FONT_HERSHEY_COMPLEX,0.5,(125,125,255),2)
 
 path = 'data/basicshapes.jpg'
 img = cv2.imread(path)
@@ -51,7 +67,7 @@ imgCanny = cv2.Canny(imgBlur, 50, 50)
 getContours(imgCanny)
 imgBlank = np.zeros_like(img)
 
-imgStack = stackImages(0.6,([img, imgGray, imgBlur],
+imgStack = stackImages(0.9,([img, imgGray, imgBlur],
                             [imgCanny, imgContour, imgBlur]))
 
 cv2.imshow("Stack image", imgStack)
